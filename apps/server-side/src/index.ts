95fc31354242d526fc4e51785d0d7d62561e2d15
Kuntalmajee2557 <kuntalmajee338@gmail.com>
2025-01-28 10:58:48 +0530

    auto route created

--- apps/server-side/src/index.ts
+++ apps/server-side/src/index.ts
@@ -1,5 +1,17 @@
 import express from "express"
+import userRouter from "./routes/user"
+import { prismaClient } from "@repo/db/client"
 const app = express()
-app.listen(3000, () => {
-    console.log("listening the port")
+app.use(express.json())
+
+const PORT = 3000;
+
+app.use("/api/v1/user", userRouter)
+
+app.get('/', (req, res) => {
+    res.send("home")
+})
+
+app.listen(PORT, () => {
+    console.log(`listening the port ${PORT}`)
 })
